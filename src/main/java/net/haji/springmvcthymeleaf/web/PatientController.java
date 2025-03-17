e932a7ff3a5b9f36787e282f41681a124b5380e9
mohammedhaji <hmohammediaji@gmail.com>
2025-03-17 17:56:01 +0000

    add pagination

--- src/main/java/net/haji/springmvcthymeleaf/web/PatientController.java
+++ src/main/java/net/haji/springmvcthymeleaf/web/PatientController.java
@@ -3,9 +3,12 @@ package net.haji.springmvcthymeleaf.web;
 import lombok.AllArgsConstructor;
 import net.haji.springmvcthymeleaf.dao.entities.Patient;
 import net.haji.springmvcthymeleaf.dao.repositories.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,9 +17,13 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patientsList", patients);
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "size", defaultValue = "4") int size) {
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("patientsList", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
         return "patients";
     }
 }
