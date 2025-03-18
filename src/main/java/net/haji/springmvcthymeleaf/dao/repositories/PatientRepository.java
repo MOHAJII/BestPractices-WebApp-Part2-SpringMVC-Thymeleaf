5158d15ea36b31601377a3ce52d6884925ee86a7
mohammedhaji <hmohammediaji@gmail.com>
2025-03-18 14:25:10 +0000

    add a search bar

--- src/main/java/net/haji/springmvcthymeleaf/dao/repositories/PatientRepository.java
+++ src/main/java/net/haji/springmvcthymeleaf/dao/repositories/PatientRepository.java
@@ -1,6 +1,8 @@
 package net.haji.springmvcthymeleaf.dao.repositories;
 
 import net.haji.springmvcthymeleaf.dao.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +16,6 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     @Query("select p from Patient p where p.name like :x")
     List<Patient> search(@Param("x") String keyword);
+
+    Page<Patient> findByNameContainsIgnoreCase(String keyword, Pageable pageable);
 }
